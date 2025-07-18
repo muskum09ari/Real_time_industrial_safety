5177c74db660c234c4fa8db510c13acb655e54d5
muskum09ari <96814106+muskum09ari@users.noreply.github.com>
2025-07-18 16:03:40 +0530

    Update and rename us_slave to us_slave.c

--- us_slave.c
+++ us_slave.c
@@ -2,45 +2,28 @@
 #include "lcd.h"
 #include <stdint.h>
 
-// Initialize USART3 on PC11 (RX) and PC10 (TX)
 void usart3_init(void) {
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
     RCC->APB1ENR |= RCC_APB1ENR_USART3EN;
 
-    // PC11 (RX)
-    GPIOC->MODER &= ~(3 << (2 * 11));
-    GPIOC->MODER |=  (2 << (2 * 11));
-    GPIOC->AFR[1] &= ~(0xF << ((11 - 8) * 4));
-    GPIOC->AFR[1] |=  (7 << ((11 - 8) * 4));  // AF7 for RX
+    GPIOC->MODER &= ~((3 << (2 * 10)) | (3 << (2 * 11)));
+    GPIOC->MODER |=  (2 << (2 * 10)) | (2 << (2 * 11));  // AF mode
+    GPIOC->AFR[1] &= ~((0xF << ((10 - 8) * 4)) | (0xF << ((11 - 8) * 4)));
+    GPIOC->AFR[1] |=  (7 << ((10 - 8) * 4)) | (7 << ((11 - 8) * 4));  // AF7 for USART3
 
-    // PC10 (TX)
-    GPIOC->MODER &= ~(3 << (2 * 10));
-    GPIOC->MODER |=  (2 << (2 * 10));
-    GPIOC->AFR[1] &= ~(0xF << ((10 - 8) * 4));
-    GPIOC->AFR[1] |=  (7 << ((10 - 8) * 4));  // AF7 for TX
-
-    USART3->BRR = 0x683;  // Baud rate 9600 (16MHz clock)
+    USART3->BRR = 0x683;  // 9600 baud @ 16MHz
     USART3->CR1 |= USART_CR1_RE | USART_CR1_TE;  // Enable RX and TX
     USART3->CR1 |= USART_CR1_UE;  // Enable USART
 }
 
-// Blocking USART3 character read
 uint8_t usart3_read_char(void) {
     while (!(USART3->SR & USART_SR_RXNE));
     return (uint8_t)USART3->DR;
 }
 
-// Blocking USART3 character write
 void usart3_send_char(char c) {
     while (!(USART3->SR & USART_SR_TXE));
     USART3->DR = c;
-}
-
-// Send a string over USART3
-void usart3_send_string(const char *str) {
-    while (*str) {
-        usart3_send_char(*str++);
-    }
 }
 
 int main(void) {
@@ -55,6 +38,7 @@ int main(void) {
     uint8_t idx = 0;
     char c;
 
+    // Read message
     while (idx < sizeof(buffer) - 1) {
         c = usart3_read_char();
         if (c == '\r' || c == '\n') break;
@@ -62,13 +46,14 @@ int main(void) {
     }
     buffer[idx] = '\0';
 
+    // Display received data
     lcd(0x01, 0);
     lcd_string("Received:");
     lcd(0xC0, 0);
     lcd_string(buffer);
 
-    // Send ACK to master
-    usart3_send_string("OK\r\n");
+    //  Send acknowledgment back
+    usart3_send_char('A');  // This tells master that slave is alive
 
     while (1);
 }
